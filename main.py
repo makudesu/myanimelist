03f45c80ba26372fa7e32454eaf741c6f680d760
Mark Kevin <makudesu@grex.org>
2016-05-24 19:44:31 +0800

    Prototype 1

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygtk, gtk
+
+class Window1:
+    """ This is just a test function """
+    def __init__(self):
+        self.gladefile = "myultimate.glade"
+        self.builder = gtk.Builder()
+        self.builder.add_from_file(self.gladefile)
+        
+        dic = { "on_btnQuit_clicked": self.btnQuit_clicked,
+                "on_btnNew_clicked": self.btnNew_clicked
+             }
+        self.builder.connect_signals(dic)
+        self.window = self.builder.get_object("window1")
+        self.window.show_all()
+        if (self.window):
+            self.window.connect("destroy", gtk.main_quit)
+
+    def btnQuit_clicked(self, widget):
+        gtk.main_quit()
+
+    def btnNew_clicked(self, widget):
+        print "btnNew_clicked is clicked"
+
+
+if __name__ == "__main__":
+    app = Window1()
+    gtk.main()
