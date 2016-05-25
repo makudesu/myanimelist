2b6a7a9455632b17971b7b94145e9aaf21212d6d
Mark Kevin <makudesu@grex.org>
2016-05-25 18:42:12 +0800

    Added Create

--- main.py
+++ main.py
@@ -21,24 +21,38 @@ class Anime(Base):
 class Window1:
     """ This is just a test function """
     def __init__(self):
+        self.session = Session()
         self.gladefile = "myultimate.glade"
         self.builder = gtk.Builder()
         self.builder.add_from_file(self.gladefile)
         
         dic = { "on_btnQuit_clicked": self.btnQuit_clicked,
-                "on_btnOpen_clicked": self.btnOpen_clicked
+                "on_btnSave_clicked": self.btnSave_clicked
              }
+
+        self.gtkName = self.builder.get_object('txtboxName')
+        self.gtkTags = self.builder.get_object('txtboxTags')
+        self.gtkPlot = self.builder.get_object('txtboxPlot')
+        self.gtkStar = self.builder.get_object('txtboxStar')
+
         self.builder.connect_signals(dic)
         self.window = self.builder.get_object("window1")
         self.window.show_all()
         if (self.window):
             self.window.connect("destroy", gtk.main_quit)
 
+    def btnSave_clicked(self, widget):
+        self.anime_on_session = Anime(
+             name=self.gtkName.get_text(), 
+             tags=self.gtkTags.get_text(),
+             plot=self.gtkPlot.get_text(),
+             star=self.gtkStar.get_text()
+        )
+        self.session.add(self.anime_on_session)
+        self.session.commit()
+
     def btnQuit_clicked(self, widget):
         gtk.main_quit()
-
-    def btnOpen_clicked(self, widget):
-        print "btnOpen_clicked is clicked"
 
 
 if __name__ == "__main__":
